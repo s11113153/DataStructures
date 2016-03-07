33704d9d84695bcc7fc1c8276c6ca1dad118335a
m043010020 <m043010020@student.nsysu.edu.tw>
2016-03-08 00:29:45 +0800

    add comment for code

--- Graph/src/com/company/Main.java
+++ Graph/src/com/company/Main.java
@@ -2,16 +2,21 @@ package com.company;
 
 public class Main {
   public static void main(String[] args) {
+
+    // example 1
     Edge edges = new Edge.Builder(new int[][] {
         {1, 2}, {2, 1}, {1, 3}, {3, 1}, {2, 3}, {3, 2},
         {2, 4}, {4, 2}, {3, 5}, {5, 3}, {4, 5}, {5, 4}
     }).build();
+    // execute example 1
     Graph.createGraph(edges, (item) -> item.print());
 
+    // example 2
     Edge edges2 = new Edge.Builder(new int[][] {
         {1, 3}, {3, 1}, {1, 4}, {4, 1}, {2, 1}, {1, 2},
         {2, 4}, {4, 2}, {2, 5}, {5, 2}, {4, 3}, {3, 4}
     }).build();
+    // execute example 2
     Graph.createGraph(edges2, (item -> item.print()));
   }
 }
